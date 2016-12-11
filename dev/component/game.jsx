5e4b4a4f7380c60841ae6f4aa8d694249f822953
iapYang <yklorse@gmail.com>
2016-12-11 22:57:56 +0800

    1211 XO unfinished

--- dev/component/game.jsx
+++ dev/component/game.jsx
@@ -2,19 +2,38 @@ import React from 'react';
 import Board from './board.jsx';
 
 class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null)
+            }],
+            xIsNext: true
+        };
+    }
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>
+                        {/* status */}
+                    </div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
 }
+
+var player = {score: 1, name: 'Jeff'};
+
+var newPlayer = Object.assign({}, player, {score: 2});
+// Now player is unchanged, but newPlayer is {score: 2, name: 'Jeff'}
+
+// Or if you are using object spread, you can write:
+// var newPlayer = {score: 2, ...player};
 
 export default Game;
--- dev/component/square.jsx
+++ dev/component/square.jsx
@@ -10,9 +10,7 @@ class Square extends React.Component {
     }
 
     clickHandler() {
-        this.setState({
-            value: 'X',
-        });
+        this.props.onClick();
     }
     render() {
         return (
@@ -20,31 +18,12 @@ class Square extends React.Component {
                 className="square"
                 onClick={this.clickHandler}>
                 <span>
-                    {this.state.value}
+                    {this.props.value}
                 </span>
             </div>
         );
     }
 }
 
-function calculateWinner(squares) {
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
-        }
-    }
-    return null;
-}
 
 export default Square;
