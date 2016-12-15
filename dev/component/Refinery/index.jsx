6560e2d0b88ab04ca7d3a73e155a2d6f2f2bff58
iapYang <yklorse@gmail.com>
2016-12-15 16:56:58 +0800

    1215 add content

--- dev/component/Refinery/index.jsx
+++ dev/component/Refinery/index.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import Header from './component/Header';
+import Content from './component/Content';
 
 class Refinery extends React.Component {
     render () {
         return (
             <div className='refinery-wrapper'>
                 <Header />
-                <div className='frame'>
-
-                </div>
+                <Content />
             </div>
         );
     }
